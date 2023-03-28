b54eb9f47248000324e9e49df47b69e88dd26a3d
Roma <92884248+saipovx@users.noreply.github.com>
2023-03-29 00:51:54 +0300

    ok

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+
+
+// Мобильное меню бургер
+function burgerMenu() {
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.menu')
+    const body = document.querySelector('body')
+    
+    burger.addEventListener('click', () => {
+
+      if (!menu.classList.contains('active')) {
+        menu.classList.add('active')
+        burger.classList.add('active-burger')
+        body.classList.add('locked')
+      } else {
+        menu.classList.remove('active')
+        burger.classList.remove('active-burger')
+        body.classList.remove('locked')
+      }
+
+    })
+
+  }
+
+  burgerMenu() ;
+
+
